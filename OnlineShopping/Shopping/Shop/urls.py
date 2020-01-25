ee34a0fb4f20fce4a2ff76c3832f4e5430aedc0c
rohit121999 <58909165+rohit121999@users.noreply.github.com>
2020-01-25 11:02:22 +0545

    new

---
+++ OnlineShopping/Shopping/Shop/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns = [
+    path("", views.index, name="index"),
+    path('viewproduct/<int:id>', views.view_product, name="view_product"),
+    path("itemdetail/",views.itemdetail,name="itemdetail"),
+    path("add-to-cart/<int:id>", views.add_to_cart, name="addtocart"),
+    path("remove-from-cart/<int:id>", views.remove_from_cart, name="removefromcart"),
+    path("increase/<int:id>", views.increase, name="increase"),
+    path("decrease/<int:id>", views.decrease, name="decrease"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
