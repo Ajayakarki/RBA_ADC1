0f5a52056032837ce76b1ce84fa54c29c862caf2
rohit121999 <58909165+rohit121999@users.noreply.github.com>
2020-01-25 11:26:52 +0545

    deleted

--- OnlineShopping/Shopping/Feedback/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class FeedBack(models.Model):
-	Name = models.CharField(max_length=20)
-	Email = models.EmailField()
-	Subject = models.CharField(max_length=20)
-	Message = models.TextField()
-
