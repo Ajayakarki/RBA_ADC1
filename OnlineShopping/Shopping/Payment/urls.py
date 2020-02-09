613930099af7f46bbcd02c8533dc8a63ad4e959d
Bzen22 <58922496+Bzen22@users.noreply.github.com>
2020-02-09 22:46:40 +0545

    delete

--- OnlineShopping/Shopping/Payment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("Checkout/", views.Checkout, name="Checkout"),
-    path("invoice/", views.invoice, name="invoice"),
-
-
-]
